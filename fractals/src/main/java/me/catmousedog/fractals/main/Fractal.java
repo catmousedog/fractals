04920baf14bf7e63a0da094e69e00bca89df2e89
'catmousedog' <'lauwersarno@hotmail.com'>
2020-06-04 01:22:03 +0200

    doing latest test build doing latest test build doing latest test build doing latest test build doing latest test build doing latest test build doing latest test build doing latest test build doing latest test build

--- fractals/src/main/java/me/catmousedog/fractals/main/Fractal.java
+++ fractals/src/main/java/me/catmousedog/fractals/main/Fractal.java
@@ -6,16 +6,19 @@ import org.jetbrains.annotations.NotNull;
 
 public class Fractal {
 	
+	private final Scalar f;
+	
 	private final int width, height;
 	
 	private final int type = BufferedImage.TYPE_INT_RGB;
 	
 	private final boolean parallel;
 	
-	public Fractal(int w, int h, boolean parallel) {
+	public Fractal(int w, int h, boolean parallel, Scalar f) {
 		width = w;
 		height = h;
 		this.parallel = parallel;
+		this.f = f;
 	}
 	
 	@NotNull
--- fractals/src/main/java/me/catmousedog/fractals/main/Main.java
+++ fractals/src/main/java/me/catmousedog/fractals/main/Main.java
@@ -3,7 +3,9 @@ package me.catmousedog.fractals.main;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		//test
 	}
+	
+	
 	
 }
