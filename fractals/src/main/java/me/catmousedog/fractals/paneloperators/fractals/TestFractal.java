b36b5dc61ed5ef544135715b30bbc8ef59f571c2
catmousedog <lauwersarno@hotmail.com>
2021-03-15 12:25:48 +0100

    test - complex function for RationFractal

--- fractals/src/main/java/me/catmousedog/fractals/paneloperators/fractals/TestFractal.java
+++ fractals/src/main/java/me/catmousedog/fractals/paneloperators/fractals/TestFractal.java
@@ -1,8 +1,5 @@
 package me.catmousedog.fractals.paneloperators.fractals;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.jetbrains.annotations.NotNull;
 
 import me.catmousedog.fractals.data.FractalValue;
@@ -13,62 +10,47 @@ import me.catmousedog.fractals.paneloperators.functions.IterativeFunction;
 import me.catmousedog.fractals.paneloperators.functions.NormalizedFunction;
 import me.catmousedog.fractals.paneloperators.functions.PotentialFunction;
 import me.catmousedog.fractals.paneloperators.functions.TestFunction;
-import me.catmousedog.fractals.ui.components.Item;
-import me.catmousedog.fractals.ui.components.concrete.SliderDouble;
 
 public class TestFractal extends MouseFractal {
 
 	public TestFractal() {
 		super();
 
-		List<Item> l = new ArrayList<Item>(m + 1);
-		l.add(new SliderDouble.Builder().setLabel("C").setMin(-10).setMax(10).setChange(c -> changePole()).build());
-		for (int i = 0; i < m; i++) {
-			l.add(new SliderDouble.Builder().setLabel(Integer.toString(i)).setMin(-3).setMax(3)
-					.setChange(c -> changePole()).build());
-		}
-		items = new Item[m + 1];
-		l.toArray(items);
-
 		functions = new Function[] { new IterativeFunction(this), new NormalizedFunction(this),
 				new PotentialFunction(this), new EscapeAngleFunction(this), new BinaryFunction(this),
 				new TestFunction(this) };
 		function = functions[0];
+
+		for (int i = 0; i < n; i++) {
+			double t = T * i / (double) (n + 1);
+			Complex z = Complex.exp(t);
+			r[i] = f(z);
+		}
+
+		jx = 1.0;
+		jy = 0.0;
 	}
 
 	private TestFractal(TestFractal fractal) {
 		super(fractal);
-
-		for (int j = 0; j < m; j++) {
-			t[j] = fractal.t[j];
-			T[j] = fractal.T[j];
+		for (int i = 0; i < n; i++) {
+			r[i] = fractal.r[i];
 		}
-		C = fractal.C;
 	}
 
-	// poles
-	private int m = 2;
-	private Complex[] t;
-	// powers
-	private double[] T;
-	// extra parameter
-	private double C = Math.exp(0);
+	private double T = 2 * Math.PI;
+	private int n = 400;
+	private Complex[] r = new Complex[n];
 
-	{
-		jx = 1;
-		t = new Complex[m];
-		T = new double[m];
-
-		t[0] = new Complex(1, 0);
-		t[1] = new Complex(-1, 0);
-		for (int j = 0; j < m; j++) {
-			T[j] = 1;
-		}
+	private Complex f(Complex z) {
+		return new Complex(Math.sin(z.x) * Math.cosh(z.y), Math.cos(z.x) * Math.sinh(z.y));
 	}
 
 	@Override
 	public FractalValue get(double cx, double cy) {
 		Complex q = new Complex(cx, cy);
+
+		Complex C = new Complex(jx, jy);
 
 		for (int i = 0; i < iterations; i++) {
 
@@ -76,39 +58,20 @@ public class TestFractal extends MouseFractal {
 				return new FractalValue(q.x, q.y, 0, 0, i, iterations);
 			}
 
-			Complex PT = new Complex(1, 0), PB = new Complex(1, 0);
-			for (int j = 0; j < m; j++) {
-				PT.multiply(q.poly(t[j], T[j]));
-			}
+//			for (int j = 0; j < n; j++) {
+//				if (q.subtract(r[j]).power(2.0).mag() < 0.001)
+//					return new FractalValue(0, 0, 0, 0, iterations, iterations);
+//			}
 
-			q = PT;
-			q.multiply(new Complex(C, 0));
-			q.divide(PB);
+			Complex temp = new Complex(1, 0);
+			for (int j = 0; j < n; j++) {
+				temp = temp.multiply(q.subtract(r[j]));
+			}
+			temp = temp.multiply(C.power(-n));
+			temp = q.multiply(temp.add(new Complex(1, 0)));
+			q = temp;
 		}
 		return new FractalValue(0, 0, 0, 0, iterations, iterations);
-	}
-
-	@Override
-	public void save() {
-		super.save();
-
-		C = Math.exp(((SliderDouble) items[0]).saveAndGet());
-		for (int i = 0; i < m; i++)
-			T[i] = ((SliderDouble) items[i + 1]).saveAndGet();
-	}
-
-	@Override
-	public void update() {
-		super.update();
-		
-		((SliderDouble) items[0]).setDataSafe(Math.log(C));
-		for (int i = 0; i < m; i++)
-			((SliderDouble) items[i + 1]).setDataSafe(T[i]);
-	}
-
-	private void changePole() {
-		saveAndRender();
-		update();
 	}
 
 	@Override
@@ -131,19 +94,12 @@ public class TestFractal extends MouseFractal {
 		return new TestFractal(this);
 	}
 
-	private class Complex {
+	private static class Complex {
 		private double x, y;
 
 		public Complex(double x, double y) {
 			this.x = x;
 			this.y = y;
-		}
-
-		public Complex poly(Complex t, double T) {
-			Complex c = new Complex(x, y);
-			c.subtract(t);
-			c.power(T);
-			return c;
 		}
 
 		public Complex divide(Complex c) {
@@ -153,23 +109,30 @@ public class TestFractal extends MouseFractal {
 			return new Complex(tx, ty);
 		}
 
-		public void multiply(Complex c) {
+		public Complex multiply(Complex c) {
 			double tx = x * c.x - y * c.y;
-			y = y * c.x + x * c.y;
-			x = tx;
+			double ty = y * c.x + x * c.y;
+			return new Complex(tx, ty);
 		}
 
-		public void subtract(Complex c) {
-			x -= c.x;
-			y -= c.y;
+		public Complex subtract(Complex c) {
+			return new Complex(x - c.x, y - c.y);
 		}
 
-		public void power(double a) {
+		public Complex add(Complex c) {
+			return new Complex(x + c.x, y + c.y);
+		}
+
+		public Complex power(double a) {
 			double theta = Math.atan2(y, x);
-//			double Z = Math.pow(x * x + y * y, a / 2);
 			double Z = Math.exp(Math.log(x * x + y * y) * a / 2);
-			x = Math.cos(a * theta) * Z;
-			y = Math.sin(a * theta) * Z;
+			double tx = Math.cos(a * theta) * Z;
+			double ty = Math.sin(a * theta) * Z;
+			return new Complex(tx, ty);
+		}
+		
+		public static Complex exp(double a) {
+			return new Complex(Math.cos(a), Math.sin(a));
 		}
 
 		public double mag() {
